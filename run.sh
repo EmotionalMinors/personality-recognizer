aa561199dc60837cd55f54710d327e2b25682c7a
Bobby Dhillon <bosdhill@ucsc.edu>
2019-11-30 00:26:37 -0800

    updated readme and refactored python files

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+error=$(mkdir data/txt 2>&1)
 if [[ $1 == "csv" ]]; then
-	python3 python3/csv_to_txt.py
+	python3 python/csv_to_txt.py
 else
 if [[ $1 == "arff" ]]; then
-	python3 python3/arff_to_csv.py
+	python3 python/arff_to_csv.py
 else
 if [[ $1 == "rec" ]]; then
 	./PersonalityRecognizer -i data/txt -d -a cleanset.arff -t 2 -m 4
 else 
-	python3 python3/csv_to_txt.py
+	python3 python/csv_to_txt.py
 	./PersonalityRecognizer -i data/txt -d -a cleanset.arff -t 2 -m 4
-	python3 python3/arff_to_csv.py
+	python3 python/arff_to_csv.py
 fi
 fi
 fi
